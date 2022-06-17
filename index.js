e91d2d063e5a4ac768ac9d5a8f259d2b4d40a118
Akezhan <akezhan.issadilov@gmail.com>
2022-06-17 22:12:04 +0600

    Build created

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const app = express()
 
 app.use(express.json({extended: true}))
 
-const PORT = config.get("port")
-
 app.use('/api/auth', require('./routes/authorization-routes'))
 app.use('/api/articles', require('./routes/article-routes'))
+
+if(process.env.NODE_ENV==='production'){
+    app.use('/',express.static(path.join(__dirname,'frontend','build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'frontend','build','index.html'))
+    })
+}
+
+const PORT = process.env.PORT || config.get("port")
 
 async function start(){
     try{
